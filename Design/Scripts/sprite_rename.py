8bb2b89a315a2f3458a7b9ac8ae373e309434f13
Ray Taylor Lin <raytaylorlin@gmail.com>
2018-03-17 00:05:58 +0800

    添加魔里莎资源

--- Design/Scripts/sprite_rename.py
+++ Design/Scripts/sprite_rename.py
@@ -1,15 +1,29 @@
-filename = 'reimu.png.meta'
-prefix = 'reimu_'
+# filename = 'reimu.png.meta'
+# prefix = 'reimu_'
+# replace_info = [
+#     ['idle_', 0, 10],
+#     ['attack1_', 11, 20],
+#     ['attack2_', 21, 30],
+#     ['hit_', 31, 33],
+#     ['down_', 34, 44],
+#     ['useitem_', 45, 55],
+#     ['spell1_', 56, 67],
+#     ['spell2_', 68, 82]
+# ]
+
+filename = 'marisa.png.meta'
+prefix = 'marisa_'
 replace_info = [
-    ['idle_', 0, 10],
-    ['attack1_', 11, 20],
-    ['attack2_', 21, 30],
-    ['hit_', 31, 33],
-    ['down_', 34, 44],
-    ['useitem_', 45, 55],
-    ['spell1_', 56, 67],
-    ['spell2_', 68, 82]
+    ['idle_', 0, 9],
+    ['attack1_', 10, 19],
+    ['attack2_', 20, 33],
+    ['hit_', 34, 36],
+    ['down_', 37, 43],
+    ['useitem_', 44, 53],
+    ['spell1_', 54, 77],
+    ['spell2_', 78, 93]
 ]
+
 
 content = open(filename).read()
 for info in replace_info:
